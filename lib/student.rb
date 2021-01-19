2105996aace557acc7c18e9324bf906de32e75e4
Allison _ <allimacq@gmail.com>
2021-01-19 20:37:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,7 +96,8 @@ class Student
   end
   
   def self.first_student_in_grade_10
-    puts self.first_X_students_in_grade_10(1).id
+    first_student = self.first_X_students_in_grade_10(1)
+    puts first_student.id
   end
   
 end
