322d410a23e5c02322a9177f1b7b713d80782fda
Allison _ <allimacq@gmail.com>
2021-01-19 20:30:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,12 +88,11 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT number = ?
     SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
+    end.take(number)
   end
   
 end
