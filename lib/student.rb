92b88695beace1092f98fe0c705013c053a2c9f7
Allison _ <allimacq@gmail.com>
2021-01-19 20:34:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,8 +96,7 @@ class Student
   end
   
   def self.first_student_in_grade_10
-    puts self
-    #self.first_X_students_in_grade_10(1)
+    puts self.first_X_students_in_grade_10(1)
   end
   
 end
