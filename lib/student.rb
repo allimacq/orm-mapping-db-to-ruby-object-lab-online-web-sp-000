7309eebe9e031b9a0f14dfdfd1ed66e3781ca6b8
Allison _ <allimacq@gmail.com>
2021-01-19 21:22:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,7 +97,7 @@ class Student
   
   def self.first_student_in_grade_10
     first_student = self.first_X_students_in_grade_10(1)
-    p first_student.to_s
+    p first_student.to_s.split
   end
   
   def self.all_students_in_grade_X(grade)
