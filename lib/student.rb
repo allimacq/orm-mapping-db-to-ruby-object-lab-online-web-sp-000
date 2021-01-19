182c7960a57619e0fe7d479f713e59c0d770c104
Allison _ <allimacq@gmail.com>
2021-01-19 21:18:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,6 +97,7 @@ class Student
   
   def self.first_student_in_grade_10
     first_student = self.first_X_students_in_grade_10(1)
+    first_student.join(",")
   end
   
   def self.all_students_in_grade_X(grade)
